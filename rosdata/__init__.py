fd0f48728aab20ca1f88562461b3cf91ffa5f647
jmount1992 <jmount1992@gmail.com>
2022-09-29 16:54:56 +1000

    Added exception catching to allow access to CSVROSData without requiring all the ros tools installed.

--- rosdata/__init__.py
+++ rosdata/__init__.py
@@ -4,6 +4,6 @@ import warnings
 try:
     from rosdata.core import ROSBagExtractor, ROSBagTransformer, TransformStatus
 except ImportError as e:
-    warnings.warn("Unable to import ROSBagExtractor, ROSBagTransform, and TransformStatus from the ROSData Python Package. This was due to Import Error %s.")
+    warnings.warn("Unable to import ROSBagExtractor, ROSBagTransform, and TransformStatus from the ROSData Python Package. This was due to Import Error %s. The CSVROSData file reader class is still available.")
 
 from rosdata.utils.datareader import CSVROSData 
